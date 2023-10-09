a1a76962c41cee444af4bb0a09eb86c2aa4d593d
Taehyoun Kim <kth9903@gmail.com>
2023-10-09 21:40:45 +0900

    add mysql crud sql study file

---
+++ mysql_crud.sql
@@ -0,0 +1,10 @@
+create table pet3(
+name varchar(100) default 'unnamed',
+age int default 99);
+
+insert into pet2(name,age)values('kim',14);
+
+select * from pet2;
+create TABLE shop(name varchar(100));
+insert into shop(name) values('mario\'s emporium');
+select * from shop;
